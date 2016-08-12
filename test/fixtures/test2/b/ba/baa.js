220a03d751e72fe25a48bbba46555f3e3dc0a6e5
Clément Pasquier <toxynell@gmail.com>
2016-08-13 01:37:32 +0200

    add "sanitize" option

---
+++ test/fixtures/test2/b/ba/baa.js
@@ -0,0 +1,3 @@
+module.exports = {
+  baaa: true
+};
--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ const {describe, it, before} = exports.lab = Lab.script();
 
 describe('Test manifest building', () => {
 
-  it('should gather config files into one single object (only one path)', done => {
+  it('should merge config files into one single object (from one path)', done => {
 
     const expected = {
       a: ['cat', 'dog', 'fish'],
@@ -29,9 +29,16 @@ describe('Test manifest building', () => {
     done()
   });
 
-  it('should gather config files into one single object (multiple paths)', done => {
+  it('should merge config files into one single object (from multiple paths)', done => {
 
-    const expected = {
+    const paths = [
+      Path.join(__dirname, 'fixtures', 'test1'),
+      Path.join(__dirname, 'fixtures', 'test2')
+    ];
+
+    let result = Uhu.stick(paths);
+
+    expect(result).to.equal({
       a: {aa: 1},
       b: {
         ba: {
@@ -42,13 +49,69 @@ describe('Test manifest building', () => {
         },
         bb: {response: 42}
       }
-    };
+    });
 
-    const result = Uhu.stick(Path.join(__dirname, 'fixtures', 'test1'), Path.join(__dirname, 'fixtures', 'test2'));
 
-    expect(result).to.equal(expected);
+
+    result = Uhu.stick([
+      Path.join(__dirname, 'fixtures', 'test1'),
+      Path.join(__dirname, 'fixtures', 'test2'),
+      Path.join(__dirname, 'fixtures', 'test3')
+    ]);
+
+    expect(result).to.equal({
+      a: {aa: 1},
+      b: {
+        ba: {
+          baa: {
+            baaa: true,
+            baab: {
+              "z": "foo",
+              "y": "bar"
+            }
+          },
+          bab: {baba: 'cool'}
+        },
+        bb: {response: 42}
+      }
+    });
+
     done()
   });
 
+  //it('should merge config files into one single object (from multiple paths)', done => {
+  it('should only return keys that match the mask if "sanitize" options is provided', done => {
+
+    const mask = {
+      b: {
+        ba: {
+          baa: {
+            baab: {
+              z: true
+            }
+          }
+        }
+      }
+    };
+
+    const result = Uhu.stick([
+      Path.join(__dirname, 'fixtures', 'test1'),
+      Path.join(__dirname, 'fixtures', 'test2'),
+      Path.join(__dirname, 'fixtures', 'test3')
+    ], {sanitize: mask});
+
+    expect(result).to.equal({
+      b: {
+        ba: {
+          baa: {
+            baab: {
+              "z": "foo"
+            }
+          }
+        }
+      }
+    });
+    done()
+  });
 
 });
